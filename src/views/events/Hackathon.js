e2b6365d32a590a376bfdfb60ffbae154cdba952
david <05779@farnborough.ac.uk>
2023-08-29 12:04:06 +0100

    Updated Hackathon.js

--- src/views/events/Hackathon.js
+++ src/views/events/Hackathon.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import SponsorBanner from './../../components/SponsorBanner';
 
+//once the timer starts it'll run forever so if you leave the page you'll get errors cause it'll try change things that aren't there
+//couldn't get any sort of unload function to work to make it stop but if you have nay ideas please try
+
 const Hackathon = () => {
   function timer() {
     let countDownDate = new Date("May 1, 2024 0:0:0").getTime();
