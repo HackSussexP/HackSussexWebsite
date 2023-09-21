d376ada0a814040d0f926ec89e25d1503725dab5
Adnan <54761886+AdnanTech@users.noreply.github.com>
2023-09-21 16:24:19 +0100

    feat: sponsors component

--- src/views/events/Hackathon.js
+++ src/views/events/Hackathon.js
@@ -5,6 +5,7 @@ import EventWhereWhen from '../../components/EventWhereWhen';
 import Timer from './../../components/Timer';
 import EventAbout from './../../components/EventAbout';
 import Schedule from './../../components/Schedule';
+import Sponsors from './../../components/Sponsors';
 
 
 import background from '../../assets/hackathon-background-darker.png';
@@ -57,6 +58,37 @@ var schedule = {
   ],
 }
 
+const sponsorData = [
+  {
+    title: "Title Sponsor",
+    size: "80%",
+    logos: [hackathonLogo]
+  },
+  {
+    title: "Gold Sponsors",
+    size: "40%",
+    logos: [hackathonLogo, hackathonLogo]
+  },
+  {
+    title: "Silver Sponsors",
+    size: "30%",
+    logos: [hackathonLogo, hackathonLogo, hackathonLogo]
+  },
+  {
+    title: "Bronze Sponsors",
+    size: "20%",
+    logos: [hackathonLogo, hackathonLogo, hackathonLogo, hackathonLogo]
+  },
+  {
+    title: "Partners",
+    size: "20%",
+    logos: [hackathonLogo, hackathonLogo, hackathonLogo, hackathonLogo]
+  },
+];
+
+<Sponsors sponsorData={sponsorData} />
+
+
 var hackathonStyle = {
   backgroundImage: `url(${background})`,
   backgroundSize: 'cover',
@@ -83,6 +115,7 @@ const Hackathon = () => {
       <EventAbout />
       <EventWhereWhen location="University of Sussex, Brighton" date="June 01, 2024, 10:00AM" />
       <Schedule schedule={schedule} />
+      <Sponsors sponsorData={sponsorData}/>
     </>
   );
 }
