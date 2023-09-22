e18a27242692144ca36fb5e0684c7a19c38c1736
Charlotte-R <49107073+Charlotte-R-01134@users.noreply.github.com>
2023-09-22 15:21:12 +0100

    style: used BS 5 terms

--- src/components/PhotoModal.js
+++ src/components/PhotoModal.js
@@ -13,7 +13,7 @@ const PhotoModal = (props) => {
                     <div className="modal-content">
                         <div className="modal-header">
                             <p className="modal-title h5 text-black" id="photoModalLabel">{props.description}</p>
-                            <button type="button" className="close" data-bs-dismiss="modal" aria-label="Close">
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close">
                                 <span aria-hidden="true">&times;</span>
                             </button>
                         </div>
