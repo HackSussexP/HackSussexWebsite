f978d6f62f43e3b126b774c0bf4661a85bb79bc7
Charlotte-R <49107073+Charlotte-R-01134@users.noreply.github.com>
2023-09-22 20:49:12 +0100

    feat: error page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import React, { Suspense, lazy } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ScrollToTop from './components/ScrollToTop';
+import { Link } from 'react-router-dom';
 
 import Navbar from "./components/NewNavbar.js"; // what is this error for 
 import Footer from "./components/Footer.js";
@@ -21,8 +22,31 @@ const FAQ = lazy(() => import("./views/FAQ.js"));
 
 function NoMatch() {
   return (
-    <div>
-      <h1>404</h1>
+    <div className="container-fluid gradient opacity-75">
+      <div className="row align-items-center me-auto ms-auto vh-100">
+        <div className="col-12 text-center">
+          <h1 className="display-1 text-dark">
+            404 Error
+           </h1>
+          <h2 className="display-6 text-dark row align-items-center text-center justify-content-center">
+            <div className="col-auto p-0 m-0 text-end">
+              <i className="fas fa-coffee text-dark"> </i>
+            </div>
+            <div className="col-8 col-md-4 p-0 m-0">
+              This page is taking a coffee break.
+            </div>
+            <div className="col-auto p-0 m-0">
+              <i className="fas fa-coffee text-dark"> </i>
+            </div>
+          </h2>
+          <h4 className="text-dark">
+            In the meantime, grab your favourite mug and explore our other pages while this one takes a break.
+          </h4>
+          <Link to="/" type="button" className="btn text-dark fs-3 mt-5">
+            <i className="fas fa-arrow-left"></i> Go back
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
