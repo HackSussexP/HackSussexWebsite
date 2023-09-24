cb0c4c706467abf38154eb8ee269c1037cf48caf
Adnan <adnanquisar0@gmail.com>
2023-09-24 14:47:16 +0100

    feat: social media banner responsiveness

--- src/components/SocialMediaBanner.js
+++ src/components/SocialMediaBanner.js
@@ -1,14 +1,5 @@
 import React from 'react';
-
 import { FaDiscord, FaInstagram, FaLinkedin, FaTwitter, FaYoutube, FaTiktok } from 'react-icons/fa';
-
-const sectionStyle = {
-  backgroundSize: 'cover',
-  backgroundPosition: 'center',
-  backgroundRepeat: 'no-repeat',
-  height: '100%',
-  width: '100%',
-};
 
 const SocialMediaBanner = () => {
 
@@ -18,39 +9,39 @@ const SocialMediaBanner = () => {
 
   return (
     <>
-      <div className="container-fluid social-media-banner p-3 text-center" style={sectionStyle}>
+      <div className="container-fluid social-media-banner p-3 text-center">
         <div className="row text-center">
           <h2 className="col-12">Find us on Social Media</h2>
         </div>
         <div className="row justify-content-center p-2">
-          <div className="col col-sm-2 col-lg-1">
+          <div className="col-6 col-md-2 col-lg-1 mb-3 mb-lg-0">
             <button className="btn p-0 icon-button" onClick={() => openLink('https://discord.gg/Uwsbxy8Bqf')}>
-              <FaDiscord className="social-icon discord-icon display-2" />
+              <FaDiscord className="social-icon discord-icon display-1 mobile-big" />
             </button>
           </div>
-          <div className="col col-sm-2 col-lg-1">
+          <div className="col-6 col-md-2 col-lg-1 mb-3 mb-lg-0">
             <button className="btn p-0 icon-button" onClick={() => openLink('https://www.instagram.com/hacksussex')}>
-              <FaInstagram className="social-icon instagram-icon display-2" />
+              <FaInstagram className="social-icon instagram-icon display-1 mobile-big" />
             </button>
           </div>
-          <div className="col col-sm-2 col-lg-1">
-            <button className="btn p-0 icon-button" onClick={() => openLink('https://uk.linkedin.com/company/hacksussex')}>
-              <FaLinkedin className="social-icon linkedin-icon display-2" />
-            </button>
-          </div>
-          {/* <div className="col col-sm-2 col-lg-1">
+          <div className="col-6 col-md-2 col-lg-1 mb-3 mb-lg-0">
             <button className="btn p-0 icon-button" onClick={() => openLink('https://twitter.com/HackSussex')}>
-              <FaTwitter className="social-icon twitter-icon display-2" />
-            </button>
-          </div> */}
-          <div className="col col-sm-2 col-lg-1">
-            <button className="btn p-0 icon-button" onClick={() => openLink('https://www.youtube.com/@HackSussex')}>
-              <FaYoutube className="social-icon youtube-icon display-2" />
+              <FaTwitter className="social-icon twitter-icon display-1 mobile-big" />
             </button>
           </div>
-          <div className="col col-sm-2 col-lg-1">
+          <div className="col-6 col-md-2 col-lg-1 mb-3 mb-lg-0">
+            <button className="btn p-0 icon-button" onClick={() => openLink('https://uk.linkedin.com/company/hacksussex')}>
+              <FaLinkedin className="social-icon linkedin-icon display-1 mobile-big" />
+            </button>
+          </div>
+          <div className="col-6 col-md-2 col-lg-1 mb-3 mb-lg-0">
+            <button className="btn p-0 icon-button" onClick={() => openLink('https://www.youtube.com/@HackSussex')}>
+              <FaYoutube className="social-icon youtube-icon display-1 mobile-big" />
+            </button>
+          </div>
+          <div className="col-6 col-md-2 col-lg-1 mb-3 mb-lg-0">
             <button className="btn p-0 icon-button" onClick={() => openLink('https://www.tiktok.com/@hacksussex')}>
-              <FaTiktok className="social-icon tiktok-icon display-2" />
+              <FaTiktok className="social-icon tiktok-icon display-1 mobile-big" />
             </button>
           </div>
         </div>
@@ -59,19 +50,20 @@ const SocialMediaBanner = () => {
         .social-icon {
           color: #3498db;
           transition: color 0.5s;
-          margin-left: 24px;
-          margin-right: 24px;
         }
         .discord-icon:hover,
         .instagram-icon:hover,
         .linkedin-icon:hover,
-        .twitter-icon:hover,
         .youtube-icon:hover,
         .tiktok-icon:hover {
           color: #34d2c8;
         }
+        @media (max-width: 768px) {
+          .mobile-big {
+            font-size: 5rem;
+          }
+        }
       `}</style>
-
     </>
   )
 }
