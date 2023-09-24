9d78c4603033c1c14b76296c71527a1ae25f6704
Adnan <adnanquisar0@gmail.com>
2023-09-24 20:03:58 +0100

    chore: Game Jam layout

--- src/views/events/GameJam.js
+++ src/views/events/GameJam.js
@@ -1,9 +1,101 @@
 import React from 'react';
+
 import GameJamLogo from './../../assets/index-carousel/gamejam-logo.png';
 import GameJamBackground from './../../assets/index-carousel/gamejam-background.png';
 
+import EventWhereWhen from '../../components/EventWhereWhen'; 
+import Timer from './../../components/Timer';
+import Schedule from './../../components/Schedule';
+import Sponsors from './../../components/Sponsors';
+
+import AboutSection from '../../components/AboutSection';
+
+const config = {
+  "timer": false,
+  "event": false,
+  "schedule": false,
+  "sponsors": false,
+  "date": "May 1, 2024, 0:0:0",
+}
+
+var schedule = {
+  1: [
+    {
+      time: "10:00",
+      event: "Registration Opens",
+      location: "Chichester Lecture Theatre",
+    },
+    {
+      time: "11:00 - 12:00",
+      event: "Opening Ceremony",
+      location: "Chichester Lecture Theatre",
+    },
+    {
+      time: "12:00",
+      event: "Hacking Begins",
+      location: "The beginning of the journey",
+    },
+    {
+      time: "1:00PM",
+      event: "Event 4",
+      location: "Location 1",
+    }
+  ],
+  2: [
+    {
+      time: "10:00AM",
+      event: "Event 1",
+      location: "Location 1",
+    },
+    {
+      time: "11:00AM",
+      event: "Event 2",
+      location: "Location 1",
+    },
+    {
+      time: "12:00PM",
+      event: "Event 3",
+      location: "Location 1",
+    },
+    {
+      time: "1:00PM",
+      event: "Event 4",
+      location: "Location 1",
+    }
+  ],
+}
+
+const sponsorData = [
+  {
+    title: "Title Sponsor",
+    size: "80%",
+    logos: [GameJamLogo]
+  },
+  {
+    title: "Gold Sponsors",
+    size: "40%",
+    logos: [GameJamLogo, GameJamLogo]
+  },
+  {
+    title: "Silver Sponsors",
+    size: "30%",
+    logos: [GameJamLogo, GameJamLogo, GameJamLogo]
+  },
+  {
+    title: "Bronze Sponsors",
+    size: "20%",
+    logos: [GameJamLogo, GameJamLogo, GameJamLogo, GameJamLogo]
+  },
+  {
+    title: "Partners",
+    size: "20%",
+    logos: [GameJamLogo, GameJamLogo, GameJamLogo, GameJamLogo]
+  },
+];
+
 const GameJam = () => {
-  return(
+  return (
+    <>
     <div className="carousel-inner p-0">
       <img src={GameJamBackground} className="d-block w-100 h-100 img-fluid" alt='Background' />
       <div className="carousel-caption">
@@ -28,6 +120,22 @@ const GameJam = () => {
         </div>
       </div>
     </div>
+    {config.timer && <Timer date={config.date} />}
+    <AboutSection description={<>
+                  <p>
+                  This year, we are hosting our first GameJam. Collaborating with many of the local studios surrounding Brighton to focus 
+                  in on our Game Development and Design enthusiasts. Much like a hackathon, teams will be tasked with building a project to 
+                  compete in tracks to inspire game design and win prizes.If you are looking for an opportunity to collaborate with others and 
+                  experience trial and error throughout the game development process, this event would be a good match for you. Also, it will 
+                  be great for those who are looking for a place to have exploration of new technologies in the realm of game development. The 
+                  event is entirely free and paid for by our lovely sponsors.
+                  </p>
+                  </>}
+                  list={["24 Hours of project building", "Win exciting prizes", "Free swag", "Free Food and Drinks", "Workshops to learn skills"]} />
+    <EventWhereWhen location="University of Sussex, Brighton" date={config.timer === true ? config.date : null} />
+    {config.schedule && <Schedule schedule={schedule} />}
+    {/* <Sponsors sponsorData={sponsorData}/> */}
+    </>
   );
 }
 
