37af978b3759bbe61104068331feebf1f69d0045
Adnan <adnanquisar0@gmail.com>
2023-09-25 01:29:10 +0100

    feat: leetcoding resources

--- src/views/events/Leetcoding.js
+++ src/views/events/Leetcoding.js
@@ -1,9 +1,36 @@
 import React from 'react'
+
+import Resources from '../../components/Resources'
 import AboutSection from '../../components/AboutSection';
 import EventWhereWhen from '../../components/EventWhereWhen';
 
 import LeetcodingLogo from './../../assets/index-carousel/leetcoding-logo.png'
 import LeetcodingBackground from './../../assets/index-carousel/leetcoding-background.png'
+
+const linksData = [
+  // Website - Online Practice
+  { name: "LeetCode", url: "https://leetcode.com/" },
+  { name: "HackerRank", url: "https://www.hackerrank.com/" },
+  { name: "Codeforces", url: "https://codeforces.com/" },
+  { name: "AtCoder", url: "https://atcoder.jp/" },
+
+  // Website - Misc
+  { name: "Visualgo", url: "https://visualgo.net/en" },
+  { name: "Tech Interview Handbook", url: "https://www.techinterviewhandbook.org/grind75" },
+  { name: "Neeetcode", url: "https://neetcode.io/" },
+  { name: "LeetCode The Hard Way", url: "https://leetcodethehardway.com/" },
+  { name: "USACO Guide", url: "https://usaco.guide/" },
+  { name: "CP Algorithms", url: "https://cp-algorithms.com/index.html" },
+
+  // Books - DSA
+  { name: "Introduction to Algorithms", url: "https://www.amazon.co.uk/Introduction-Algorithms-Thomas-H-Cormen/dp/0262033844" },
+
+  // Books - CP
+  { name: "Competitive Programming Book", url: "https://cpbook.net/" },
+  { name: "CSES Problem Set", url: "https://cses.fi/book/book.pdf" },
+  { name: "Competitive Programming Handbook", url: "https://link.springer.com/book/10.1007/978-3-319-72547-5" },
+];
+
 
 const Leetcoding = () => {
   return (
@@ -16,6 +43,8 @@ const Leetcoding = () => {
       </div>
       <AboutSection description={`Welcome to HackSussex's weekly Leetcoding sessions! Our sessions are open to programmers of all levels, and they offer a chance to tackle a variety of engaging coding challenges. After taking a shot at these challenges, participants have the opportunity to discuss their approaches and solutions with fellow coders in a relaxed and friendly atmosphere. These sessions are an excellent way to enhance your problem-solving skills and prepare for technical job interviews in a comfortable and inclusive environment. Feel free to use your preferred programming language, and we look forward to having you join us on this coding journey at HackSussex!`} />
       <EventWhereWhen location="Future Technologies Lab, University of Sussex" date={null} />
+      <Resources links={linksData} />
+      <div className='mb-3'></div>
     </>
   )
 }
