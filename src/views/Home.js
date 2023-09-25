2756f375c83e835d535d444ddedabe8ad7769f63
Adnan <adnanquisar0@gmail.com>
2023-09-25 07:38:09 +0100

    feat: events back in the game

--- src/views/Home.js
+++ src/views/Home.js
@@ -27,7 +27,7 @@ const Home = () => {
       {/* <RotatingSponsorBanner /> */}
       <SocialMediaBanner />
       <HomeEvents />
-      {/* <UpcomingEvents /> */}
+      <UpcomingEvents />
     </>
   );
 }
