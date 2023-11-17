b39bd5860b5b0953a028cf3035d35397ed4c236a
Adnan <adnanquisar0@gmail.com>
2023-11-17 22:24:04 +0000

    feat: progress bar, background

--- src/views/events/GameJam.js
+++ src/views/events/GameJam.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React, useEffect} from 'react';
 
 import GameJamLogo from './../../assets/index-carousel/gamejam-logo.png';
 import GameJamBackground from './../../assets/index-carousel/gamejam-background.png';
@@ -19,6 +19,7 @@ import Sponsors from './../../components/Sponsors';
 import TicketSection from '../../components/TicketSection';
 
 import AboutSection from '../../components/AboutSection';
+
 
 const config = {
   "timer": true,
@@ -167,48 +168,58 @@ const sponsorData = [
 
 
 const GameJam = () => {
+  useEffect(() => {
+    document.body.classList.add("gamejam-body");
+
+    return () => {
+      document.body.classList.remove("gamejam-body");
+    };
+  }, []);
+
   return (
     <>
-    <div className="carousel-inner p-0">
-      <img src={GameJamBackground} className="d-block w-100 h-100 img-fluid splash" alt='Background' />
-      <div className="carousel-caption">
-        <div className='row align-items-center'>
-            <div className="col-12 col-lg-6">
-              <img src={GameJamLogo} alt="Game Jam Logo" className='img-fluid me-auto ms-auto d-block p-3'/>
-              <p className='text-wrap text-start d-none d-lg-block'>
-                📅 25th - 26th November 2023<br />
-                📍 Chichester building, University of Sussex, UK<br />
-                🌐 In-Person
-              </p>
-            </div>
-            <div className='col-lg-6 d-none d-lg-block'>
-              <h1 className='fw-bold display-4 text-electric'>Game Jam Event Loading...</h1>
-              <div className='mt-3'>
-                <div className="progress" style={{ height: '30px', borderRadius: '15px', backgroundColor: '#222' }}>
-                  <div className="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" style={{ width: '70%' }} aria-valuenow="70" aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-                <div className='text-center mt-2 text-light display-6'>70%</div>
+    <div className="gamejam">
+      <div className="carousel-inner p-0">
+        <img src={GameJamBackground} className="d-block w-100 h-100 img-fluid splash" alt='Background' />
+        <div className="carousel-caption">
+          <div className='row align-items-center'>
+              <div className="col-12 col-lg-6">
+                <img src={GameJamLogo} alt="Game Jam Logo" className='img-fluid me-auto ms-auto d-block p-3'/>
+                <p className='text-wrap text-start d-none d-lg-block'>
+                  📅 25th - 26th November 2023<br />
+                  📍 Chichester building, University of Sussex, UK<br />
+                  🌐 In-Person
+                </p>
               </div>
-            </div>
+              <div className='col-lg-6 d-none d-lg-block'>
+                <h1 className='fw-bold display-4 text-electric'>Game Jam Event Loading...</h1>
+                <div className='mt-3'>
+                  <div className="progress" style={{ height: '30px', borderRadius: '15px', backgroundColor: '#222' }}>
+                    <div className="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progressbar" style={{ width: '80%' }} aria-valuenow="80" aria-valuemin="80" aria-valuemax="100"></div>
+                  </div>
+                  <div className='text-center mt-2 text-light display-6'>80%</div>
+                </div>
+              </div>
+          </div>
         </div>
       </div>
+      {config.tickets && <TicketSection url={config.url} />}
+      {config.timer && <Timer date={config.date} title={config.title} />}
+      <AboutSection description={<>
+                    <p>
+                    This year, we are hosting our first GameJam. Collaborating with many of the local studios surrounding Brighton to focus 
+                    in on our Game Development and Design enthusiasts. Much like a hackathon, teams will be tasked with building a project to 
+                    compete in tracks to inspire game design and win prizes.If you are looking for an opportunity to collaborate with others and 
+                    experience trial and error throughout the game development process, this event would be a good match for you. Also, it will 
+                    be great for those who are looking for a place to have exploration of new technologies in the realm of game development. The 
+                    event is entirely free and paid for by our lovely sponsors. HackSussex GameJam 2023 is run in accordance with the <a href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf" target="_blank" rel="noopener noreferrer">MLH Code of Conduct</a>.
+                    </p>
+                    </>}
+                    list={["24 Hours of project building", "Win exciting prizes", "Free swag", "Free Food and Drinks", "Workshops to learn skills"]} />
+      <EventWhereWhen location="University of Sussex, Brighton" date={"25 November 2023"} />
+      {config.schedule && <Schedule schedule={schedule} />}
+      {config.sponsors && <Sponsors sponsorData={sponsorData} />}
     </div>
-    {config.tickets && <TicketSection url={config.url} />}
-    {config.timer && <Timer date={config.date} title={config.title} />}
-    <AboutSection description={<>
-                  <p>
-                  This year, we are hosting our first GameJam. Collaborating with many of the local studios surrounding Brighton to focus 
-                  in on our Game Development and Design enthusiasts. Much like a hackathon, teams will be tasked with building a project to 
-                  compete in tracks to inspire game design and win prizes.If you are looking for an opportunity to collaborate with others and 
-                  experience trial and error throughout the game development process, this event would be a good match for you. Also, it will 
-                  be great for those who are looking for a place to have exploration of new technologies in the realm of game development. The 
-                  event is entirely free and paid for by our lovely sponsors. HackSussex GameJam 2023 is run in accordance with the <a href="https://static.mlh.io/docs/mlh-code-of-conduct.pdf" target="_blank" rel="noopener noreferrer">MLH Code of Conduct</a>.
-                  </p>
-                  </>}
-                  list={["24 Hours of project building", "Win exciting prizes", "Free swag", "Free Food and Drinks", "Workshops to learn skills"]} />
-    <EventWhereWhen location="University of Sussex, Brighton" date={"25 November 2023"} />
-    {config.schedule && <Schedule schedule={schedule} />}
-    {config.sponsors && <Sponsors sponsorData={sponsorData} />}
     </>
   );
 }
