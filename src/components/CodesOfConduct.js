9dced2d6e0e7f4bd7ca44d79b350c2ac5c4f0bd8
david <05779@farnborough.ac.uk>
2024-01-05 20:11:50 +0000

    style: updated look of CodesOfConduct

--- src/components/CodesOfConduct.js
+++ src/components/CodesOfConduct.js
@@ -14,13 +14,13 @@ const CodesOfConduct = ({ links }) => {
   };
 
   return (
-    <div className="container-fluid justify-content-center text-center text-white" style={{backgroundColor: "var(--darker-blue)"}}>
-        <div className="row p-2">
-            <h1 className="pb-1 text fw-bold display-6 border-bottom w-50 ms-auto me-auto">This event is run is accordance with the following codes of conduct</h1>
-        </div>
-        <div className="col pb-3">
+    <div className="container-fluid text-center text-white my-4" style={{backgroundColor: "var(--darker-blue)"}}>
+        <div className="row p-2 justify-content-center">
+          <div className="col-auto">
+            <h1 className="pb-1 text fw-bold h3 ms-auto me-auto">This event is run is accordance with the following codes of conduct:</h1>
+          </div>
             {links.map((link, index) => (
-                <div key={index} className="row">
+                <div key={index} className="col-auto">
                     <a
                         style={hoveredLink === index ? hoverStyle : defaultStyle}
                         onMouseEnter={() => setHoveredLink(index)}
@@ -28,7 +28,7 @@ const CodesOfConduct = ({ links }) => {
                         className="fs-4"
                         href={link.url}
                     >
-                        {link.name}
+                      {index > 0 ? link.name : link.name + ","}
                     </a>
                 </div>
             ))}
