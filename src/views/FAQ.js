8f18c1c76467dcb814cc282157a4cd663bcb9e52
david <05779@farnborough.ac.uk>
2024-02-01 16:21:15 +0000

    update: fixed colouring and grammar on FAQ

--- src/views/FAQ.js
+++ src/views/FAQ.js
@@ -5,44 +5,40 @@ const FAQ = () => {
 
   const qa_dict = [
     {
-        question: "Since when has the University of Sussex's Society for computer science been active?",
+        question: "Since when has HackSussex been active?",
         answer: "The society has been active since 2012."
     },
     {
-        question: "What types of events and activities does the society organize?",
-        answer: "The society organizes various events, workshops, hackathons, and networking sessions."
+        question: "What types of events and activities does we organize?",
+        answer: "The society organizes various Hackathons, programming competitions, workshops and networking sessions."
     },
     {
         question: "Who has the society collaborated with other organizations over the years?",
         answer: "Key highlights include: GitHub, BCS the chartered institute for IT, Teach First, EDF, Overleaf and many more."
     },
     {
-        question: "How can someone interested in collaborating with the society get in touch?",
+        question: "How can I get in touch?",
         answer: "Interested individuals can get in touch by emailing contact@hacksussex.com."
     },
     {
         question: "What typically happens at one of your 24-hour hackathons?",
-        answer: "Across the 24 Hacking window, we run a mixture of workshops with sponsors and more social events. Notonly do we do this, we also supply you with each meal from amazing Brighton-based caterers."
+        answer: "Across the 24 Hacking window, we run a mixture of workshops with sponsors and more social events. Not only do we do this, we also supply you with each meal from amazing Brighton-based caterers."
     },
     {
-        question: "Is the hackathon suitable for both competitive and laid-back individuals?",
-        answer: "Yes, there's room for both the competitive and the laid-back at our hackathons."
+        question: "Are your events suitable for both competitive and laid-back individuals?",
+        answer: "Yes, there's room for both the competitive and the laid-back at all of our events."
     },
     {
         question: "What can participants expect to gain in terms of skills and experience at an event?",
-        answer: "You can expect to gain a range of skills, including tech and software skills, teamwork experience, and the opportunity to work on exciting projects in a creative environment."
+        answer: "You will get the chance to work on your programming skills, robotics, team working and industry networking."
     },
     {
         question: "What rules are in place for your events?",
-        answer: "We abide by the Hackathons UK and Major League Hacking Codes of Conduct"
+        answer: "We abide by the Hackathons UK Code of Conduct"
     },
     {
-        question: "Are HackSussex events only for experienced programmers?",
-        answer: "No, all of our events are open to programmers of all levels, making them suitable for both beginners and experienced coders."
-    },
-    {
-        question: "Is there any cost associated with attending any HackSussex event?",
-        answer: "No, attending a HackSussex event is free for participants, making them accessible to anyone interested in learning about electronics, mechanics, coding, and robotics."
+        question: "Are there any costs associated with attending a HackSussex event?",
+        answer: "No, attending a HackSussex event is free for participants."
     },
   ]
 
