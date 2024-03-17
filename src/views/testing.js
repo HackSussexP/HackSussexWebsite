0c11f97a607ec3a30d46a03b3094c3ff4009236e
Charlotte-R <49107073+Charlotte-R-01134@users.noreply.github.com>
2024-03-17 13:55:01 +0000

    update: started gallery update

--- src/views/testing.js
+++ src/views/testing.js
@@ -2,34 +2,72 @@ import React, { useEffect, useState } from 'react';
 import {Octokit} from 'octokit';
 
 const Testing = () => {
+
   const [images, setImages] = useState([])
 
   const getData = async () => {
     const octokit = new Octokit();
     const response = await octokit.request('GET /repos/{owner}/{repo}/contents/{path}', {
       owner: 'HackSussexP',
-      repo: 'public_assets',
-      path: 'assets'
+      repo: 'public',
+      path: ''
     })
-    const images = []
-    response.data.forEach((image) => {
-      images.push(image.download_url)
+    console.log(response.data)
+
+    // https://api.github.com/repos/HackSussexP/public/git/trees/8e7793404b8be32fc67225618759088457378aa3
+    console.log("SHa: " + response.data[0].sha)
+
+    // get child folders from the response folder
+    const children = await octokit.request('GET /repos/{owner}/{repo}/git/trees/{tree_sha}?recursive=1', {
+      owner: 'HackSussexP',
+      repo: 'public',
+      tree_sha: response.data[0].sha
     })
-    return images
+    
+    console.log(children.data.tree)
+
+    const data = {}
+    children.data.tree.forEach(item => {
+      if (item.type === "tree") {
+        const paths = item.path.split('/'); // Split path into parts
+        let current = data
+  
+        paths.forEach(path => {
+          if (!current[path]) {
+            current[path] = {}; // Create sub-dictionary if it doesn't exist
+          }
+          current = current[path]; // Move to the next level
+        })
+      }
+    })
+    console.log(data)
+
+    // remove file elements if there is a nested folder
+    // will need to be kept if number of elements is greater than 2
+    
+  
+
+
+    console.log(data)
   }
+
+    //   const imageUrls = response.data.map(image => {
+  //     if (image.download_url) {
+  //       return image.download_url
+  //     }
+  //   })
+  //   setImages(imageUrls)
+  // }
 
   useEffect(() => {
-    const res = getData()
-    setImages(res)
-  }
-  , [])
+    getData();
+  }, [])
 
-  console.log(images)
-  console.log(images.length)
+
+  //  console.log(images)
   return (
     <>
-      <h1>Testing</h1>
-      <img src={images[19]} />
+      
     </>
   );
 }
