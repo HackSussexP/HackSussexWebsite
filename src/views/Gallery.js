e464ef43473d8d1e1f2a77b2de0a7fd5a194b686
Charlotte-R-01134 <c.emily.richardson@outlook.com>
2024-12-14 19:34:59 +0000

    added shuffle, dynamic event names

--- src/views/Gallery.js
+++ src/views/Gallery.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useMemo } from 'react'
 import { Octokit } from 'octokit'
 import placeholderImage from '../assets/Gallery Placeholder.png'
 
@@ -34,17 +34,27 @@ const Gallery = () => {
   const [modalImageSrc, setModalImageSrc] = useState('')
   const url = "https://cdn.jsdelivr.net/gh/HackSussexP/public_assets@main/assets/Gallery/"
 
-  const eventDisplayNames = {
-    all: "All",
-    coderscup: "Coders' Cup",
-    codesocials: "Code Socials",
-    gamejam: "Game Jam",
-    hackathon: "Hackathon",
-    leetcoding: "Leetcoding",
-    misc: "Miscellaneous",
-    pwnsussex: "PwnSussex",
-    robotics: "Robotics",
-  }
+  const eventDisplayNames = useMemo(() => {
+    const defaultNames = {
+      all: "All",
+    };
+    const dynamicNames = events.reduce((acc, event) => {
+      acc[event] = event
+        .replace(/-/g, ' ')
+        .replace(/\b\w/g, (char) => char.toUpperCase());
+      return acc;
+    }, {});
+    return { ...defaultNames, ...dynamicNames };
+  }, [events]);
+
+  const shuffleArray = (array) => {
+    let shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]]; // Swap elements
+    }
+    return shuffledArray;
+  };
 
   const getData = async () => {
     const octokit = new Octokit()
@@ -92,7 +102,9 @@ const Gallery = () => {
       }
       return null
     }).filter(Boolean)
-    setImages(imageUrls)
+
+    // Shuffle the images before setting them
+    setImages(shuffleArray(imageUrls))
   }
 
   useEffect(() => {
