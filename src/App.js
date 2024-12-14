3d8199e0cdd56814efffe87724d08c54f0810112
Charlotte-R-01134 <c.emily.richardson@outlook.com>
2024-12-14 13:42:43 +0000

    taken out of game jam mode

--- src/App.js
+++ src/App.js
@@ -75,9 +75,9 @@ const App = () => {
 function RouterReload() {
   const navigate = useNavigate();
   useEffect(() => {
-    if (window.location.pathname === "/") {
-      navigate("/Events/gamejam");
-    }
+    // if (window.location.pathname === "/") {
+    //   navigate("/Events/gamejam");
+    // }
   }, []); {/* Update for specific events */}
   return (
     <>
