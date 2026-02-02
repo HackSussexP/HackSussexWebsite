3c296357ff85da51e2c0d063aac2e43e9ab77276
Charlotte-R <49107073+Charlotte-R-01134@users.noreply.github.com>
2026-02-02 11:24:09 +0000

    Added date

--- src/views/events/Hackathon.js
+++ src/views/events/Hackathon.js
@@ -162,7 +162,7 @@ const Hackathon = () => {
             <div className="col-12 col-lg-6">
               <img src={HackathonLogo} alt="Hackathon Logo" className="img-fluid me-auto ms-auto d-block p-3" />
               <p className='text-wrap text-start d-none d-lg-block'>
-                📅 Feb/March 2026 <br />
+                📅 28th Feb - 1st March 2026 <br />
                 📍 Chichester building, University of Sussex, UK
               </p>
             </div>
