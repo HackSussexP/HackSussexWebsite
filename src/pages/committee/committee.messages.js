24a748ee5f194f71fa723126aed033ae9bc24bad
Nathaniel Bates <nathaniel.christopher.bates@gmail.com>
2026-05-14 15:30:46 +0100

    fixes

--- src/pages/committee/committee.messages.js
+++ src/pages/committee/committee.messages.js
@@ -3,7 +3,7 @@ const messages = {
   title: "Meet Our Committee",
   pastCommitteeTitle: "Past Committee",
   ctaButton: "Join the Committee",
-  ctaText: "Coolest motherfuckers around"
+  ctaText: "Our amazing committee is responsible for organizing events, workshops and other activities that everyone can enjoy and win prizes in. If you enjoy hosting computer science related events and want to be part of a very cool society, please consider joining our committee!",
 };
 
 export default messages;
